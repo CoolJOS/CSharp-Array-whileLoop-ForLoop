7a08f161178ceb395443869d072ed2871807dd2f
Jess Otto Sørensen <jess.otto.sorensen@hotmail.com>
2022-05-03 15:59:01 +0200

    Code is cleaned up with remarks.

--- Begynder/Program.cs
+++ Begynder/Program.cs
@@ -1,47 +1,37 @@
+//Beginner lesson C#.
+//Make a matrix with the numbers 1 to 10 on top of each other 10 times
+//Using an Array, a "while-loop" and a "for-loop".
+
 using System;
 
 namespace Begynder
 {
     class Program
     {
-        static void Main() //(string[] args)
+        static void Main()
         {
-            int number1 = 1;
-            while (number1 <= 10)
+            int number1 = 0;
+            while (number1 < 10)
             {
-                //Console.WriteLine(number1);
-                   
-                            //BEMÆRK det ekstra 7 tal i numbers for at få den til at skrive det 7 tal i rækken skal koden ændres til den kode der er udnoteret
-                    int[] numbers = { 1, 2, 3, 4, 7, 6, 7, 8, 9, 10 };
+                    //REMARK! The Array is merely used as a counter. It can be numbers or strings, it doesn't matter.)            
+                int[] numbers = {0,1,2,3,4,7,6,7,8,9};
+                //string[] cars = { "Saab", "Volvo", "BMW", "Ford", "Toyota", "VW", "Hyundai", "Morris", "Jaguar", "Skoda" };
 
-                    for (int i = 1; i <= numbers.Length; i++)
-                    //for (int i = 0; i < numbers.Length; i++ )
-                    {
+                for (int i = 1; i <= numbers.Length; i++)
+                //for (int i = 0; i < numbers.Length; i++ )
+                {
                     Console.Write(i + " ");
-                    //Console.Write(numbers[i] + " ");
-                    }
+
+                    //REMARK! Just code to demo print of the arrays.
+                //Console.Write(cars[number1] + " ");
+                //Console.Write(numbers[number1] + " ");
+                }
              Console.Write("\n");
              number1++;
             }
 
+                //REMARK! This line is only to stop the code from continuing before a key is pressed.
             Console.ReadKey();
-
-            //int number2 = 1;
-            //while (number2 <= 10)
-            //{                           
-            //   
-            //   Console.WriteLine(number2);
-            //    number2++;
-            //}                         
-            //else
-            //{
-            //    Console.WriteLine("Good evening.");
-            //}
-
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            //Console.WriteLine("Hello World!");
-                                  
         }
     }
 }
